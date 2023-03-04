70fcb6f8989b3c1402c989571cacdd7211d3e378
LegitCamper <sawyerbristol@gmail.com>
2023-03-04 10:05:44 -0700

    adding current layouts

---
+++ avrdude.sh
@@ -0,0 +1,5 @@
+# For Pro Micro
+#
+stty -F /dev/ttyACM0 speed 1200
+stty -F /dev/ttyACM0 speed 115200
+sudo avrdude -p m32u4 -c avr109 -P /dev/ttyACM0 -b 1200 -U flash:w:../../../../lily58_rev1_LegitCamper.hex:h
